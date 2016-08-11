5a3369762dc9ccfc5fb20e61eaa18cb52f755b1d
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-08-11 00:38:24 +0000

    got ajax to consolelog but still a random error

--- src/js/components/Followers.jsx
+++ src/js/components/Followers.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var $ = require('jquery');
+//var GithubUser = require('GithubUser');
 
 /*
 This component displays a form where the user can enter a GitHub username
@@ -8,11 +10,35 @@ we will use react-router's history.push function to push a new URL to the histor
 This will have as an effect to navigate to a new URL, which will display the User component
 */
 var Followers = React.createClass({
-    
+    getInitialState: function() {
+        return {};
+    },
+    componentDidMount: function() {
+        var url = `https://api.github.com/users/${this.props.params.username}/followers`;
+        console.log(url);
+        
+        var that = this;
+        
+        $.getJSON(url).then(
+            function(response)  {
+                console.log(response);
+                that.setState({
+                    followers: response
+                });
+            } 
+        );
+    },
     render: function() {
+        if (!this.state.followers) {
+            return <div>LOADING FOLLOWERS...</div>;
+        }
+        
         return (
             <div className="followers-page">
-              <h3>Followers of {this.props.params.username}</h3>
+                <h2>Followers of {this.props.params.username}</h2>
+                <ul>
+                    {this.state.followers.map(/* INSERT CODE HERE TO RETURN A NEW <GithubUser/> */)}
+                </ul>
             </div>
         );
     }
